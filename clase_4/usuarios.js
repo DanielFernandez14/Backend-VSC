a90075c6082159b6d3228022dcd9d4b8f6d0d798
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-04-14 11:06:55 -0300

    clase_4.1

---
+++ clase_4/usuarios.js
@@ -0,0 +1,19 @@
+const usuarios = [
+    { id: 1, nombre: 'Ana', email: 'ana@example.com', rol: 'admin' },
+    { id: 2, nombre: 'Luis', email: 'luis@example.com', rol: 'editor' },
+    { id: 3, nombre: 'María', email: 'maria@example.com', rol: 'viewer' },
+    { id: 4, nombre: 'Pedro', email: 'pedro@example.com', rol: 'editor' },
+    { id: 5, nombre: 'Lucía', email: 'lucia@example.com', rol: 'admin' }
+];
+
+
+// recibir el input
+// procesar el input 
+// buscar la data en caso de que sea apto y tenga el permiso
+//devolverle la data al usuario
+
+//pedirle al usuario que indique que si quiere la lista de usuarios  debe indicarlo mediante el comando "getUsers"
+// validar credencial
+
+
+export { usuarios }
