8a81a8d6c5a268d61f574c39330da57ea516901b
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-06-06 12:26:37 -0300

    clase_20.1 1

---
+++ clase_20/backend/src/routes/authRouter.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+
+
+// http://localhost:1234/api/auth
+const authRouter = Router()
+
+//register
+// authRouter.post('/register', register)
+
+
+//login
+
+
+
+export { authRouter }
